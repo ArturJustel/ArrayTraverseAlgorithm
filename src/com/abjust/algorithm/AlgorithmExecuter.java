d01ea06dbde0896ab683fa2d678602b4da1de31f
ArturJustel <arturjusthtel@gmail.com>
2022-09-17 15:41:26 +0200

    Improve the algorithm to match specification

--- src/com/abjust/algorithm/AlgorithmExecuter.java
+++ src/com/abjust/algorithm/AlgorithmExecuter.java
@@ -3,9 +3,11 @@ package ArrayTraverseAlgorithm.src.com.abjust.algorithm;
 /*
  *  The following class executes the algorithm.
  */
-public class algorithmexecuter {
+public class AlgorithmExecuter {
     public static void main(String[] args) {
         final int[][] inputArray = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-        traversealgorithm.patternTraverser(inputArray, 0, inputArray.length, 0, inputArray[0].length);
+        TraverseAlgorithm algorithm = new TraverseAlgorithm();
+        String result = algorithm.patternTraverser(inputArray, 0, inputArray.length, 0, inputArray[0].length);
+        System.out.println(result);
     }
 }
---
+++ src/com/abjust/algorithm/TraverseAlgorithm.java
@@ -0,0 +1,55 @@
+package ArrayTraverseAlgorithm.src.com.abjust.algorithm;
+
+/*
+ *  The following class contains an recursive algorithm logic
+ *  that is able to represent a 2d array input in the clockwise pattern.
+ */
+public class TraverseAlgorithm {
+    private static String ANSWER = "";
+    /*
+     *  The following method takes a 2d array and 4 parameters as an input
+     *  and prints its values in the clockwise pattern.
+     */
+    public String patternTraverser(int[][] a2dArray,int aSr, int aEr, int aSc, int aEc){
+
+        if(aSr>=aEr || aSc>=aEc) //check if the values are not out of 2d array boundaries.
+        {
+            return ANSWER.substring(0, ANSWER.length() - 3); // remove last ", " from the answer.
+        }
+
+            for(int i = aSr;i<aEc;i++) //determine the top row and add it to the answer string.
+            {
+                ANSWER=ANSWER+a2dArray[aSr][i]+",";
+            }
+            ANSWER=ANSWER+" ";
+            aSr++;
+
+            if(aSr<aEr-1)
+            {
+                for(int i=aSr ;i<aEr;i++) //determine the right column and add it to the answer string.
+                {
+                    ANSWER=ANSWER+a2dArray[i][aEc-1]+",";
+                }
+                ANSWER=ANSWER+" ";
+                aEc--;   
+            }
+
+                for(int i=aEc-1;i>=aSc;i--) //determine the bottom row and add it to the answer string.
+                {
+                    ANSWER=ANSWER+a2dArray[aEr-1][i]+",";
+                }
+                ANSWER=ANSWER+" ";
+                aEr--;
+
+            if(aSc<aEc) //determine the left column and add it to the answer string.
+            {
+                for(int i=aEr-1 ;i>=aSr;i--)
+                {
+                    ANSWER=ANSWER+a2dArray[i][aSc]+",";
+                }
+                ANSWER=ANSWER+" ";
+                aSc++;
+            } 
+            return patternTraverser(a2dArray,aSr,aEr,aSc,aEc);
+        }
+}
